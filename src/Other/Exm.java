e5108aef929b02c340f47fc438dba08bdc208087
klein <790633566@qq.com>
2020-03-22 19:34:17 +0800

    tree depth, balanced tree

---
+++ src/Other/Exm.java
@@ -0,0 +1,8 @@
+package Other;
+
+public class Exm {
+
+    public static void main(String[] args) {
+        System.out.println(Math.round(3.896));
+    }
+}
---
+++ src/Tree/TreeLinkNode.java
@@ -0,0 +1,11 @@
+package Tree;
+
+public class TreeLinkNode {
+    int nodeData;
+    TreeLinkNode left;
+    TreeLinkNode right;
+
+    public TreeLinkNode(int nodeData) {
+        this.nodeData = nodeData;
+    }
+}
