9ecb1b85895c3901289af7effb60b47189db6b21
klein <790633566@qq.com>
2020-06-06 23:08:32 +0800

    BlockingQueue

--- src/MultiThread/BlockingQueues.java
+++ src/MultiThread/BlockingQueues.java
@@ -1,6 +1,12 @@
 package MultiThread;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class BlockingQueues {
     public static void main(String[] args) {
