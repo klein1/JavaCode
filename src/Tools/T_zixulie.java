410f77231d213fe8aac627049d3463423961db08
klein1 <wangmh1995@163.com>
2020-07-27 15:59:58 +0800

    palindrome

--- src/Tools/T_zixulie.java
+++ src/Tools/T_zixulie.java
@@ -1,9 +1,14 @@
 package Tools;
 
+import java.util.Arrays;
+
 public class T_zixulie {
     public static void main(String[] args) {
-        int[] a = new int[]{5, 2, 6, 9};
-        subList(a);
+        int[] a = new int[]{5, 2, 6, 9, 6};
+//        subList(a);
+        System.out.println(longestPalindrome(a));
+
+        System.out.println(longestPalindromeSubseq("cbbd"));
     }
 
     //连续子序列
@@ -63,5 +68,50 @@ public class T_zixulie {
             }
         }
         return maxSum;
+    }
+
+    //最长回文子串
+    private static String longestPalindrome(int[] s) {
+        int n = s.length;
+        boolean [][]dp = new boolean[n][n];
+        StringBuilder ans = new StringBuilder();
+
+        for (int l = 0; l < n; ++l) {
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = true;
+                }
+                else if (l == 1) {
+                    dp[i][j] = (s[i] == s[j]);
+                }
+                else {
+                    dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] && l + 1 > ans.length()) {
+                    ans.delete(0, ans.length());
+                    for (int i1 = i; i1 < i+l+1; i1++) {
+                        ans.append(s[i1]);
+                    }
+                }
+            }
+        }
+        return ans.toString();
+    }
+
+    // 最长回文子序列
+    private static int longestPalindromeSubseq(String s) {
+        int len = s.length();
+        int [][] dp = new int[len][len];
+        for(int i = len - 1; i>=0; i--){
+            dp[i][i] = 1;
+            for(int j = i+1; j < len; j++){
+                if(s.charAt(i) == s.charAt(j))
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                else
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+            }
+        }
+        return dp[0][len-1];
     }
 }
